0368f74994427160df9758dbacec0e4fcc3494bc
Konstantin Prusakov <konstantin.prusakov@phystech.edu>
2024-05-06 19:29:10 +0300

    add MainWindow class

---
+++ laser_beam_measurements/widgets/main/main_window.py
@@ -0,0 +1,148 @@
+# 
+# Project: laser_beam_measurements
+#
+# File: main_window.py
+#
+# Author: Konstantin Prusakov
+#
+# Copyright 2024 Konstantin Prusakov <konstantin.prusakov@phystech.edu>
+#
+
+# pyside6-uic laser_beam_measurements/widgets/main/main_window.ui -o laser_beam_measurements/widgets/main/ui_main_window.py
+
+from PySide6.QtWidgets import QMainWindow, QWidget, QMdiSubWindow
+from PySide6.QtGui import QIcon
+from PySide6.QtCore import Signal, Slot, Qt
+
+from .ui_main_window import Ui_MainWindow
+
+from laser_beam_measurements.widgets.camera_control.camera_display import CameraDisplay
+from laser_beam_measurements.widgets.camera_control.camera_select_dialog import CameraSelectDialog
+from laser_beam_measurements.main.main_object import MainObject
+from laser_beam_measurements.camera_control.camera_listener import CameraListener
+from laser_beam_measurements.camera_control.camera_listener_base import CameraState
+from laser_beam_measurements.camera_control.camera_selector import CameraSelector
+
+import laser_beam_measurements.icons.rc_icons
+
+
+class Icons:
+    start = QIcon(u":/icons/svg/play-button.svg")
+    stop = QIcon(u":/icons/svg/stop-button.svg")
+    pause = QIcon(u":/icons/svg/pause-button.svg")
+    camera = QIcon(u":/icons/svg/camera.svg")
+    display = QIcon(u":/icons/svg/display-button.svg")
+
+
+class MainWindow(QMainWindow):
+
+    signal_camera_grabber_status = Signal(bool)
+    signal_camera_close = Signal()
+
+    def __init__(self, main_object: MainObject, parent=None):
+        super().__init__(parent)
+        widget = QWidget()
+        self.setCentralWidget(widget)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self._main_object: MainObject = main_object
+        self._icons = Icons()
+        self._camera_display: CameraDisplay | None = None
+
+        self._set_icons()
+        self._connect_signals()
+
+    def _set_icons(self):
+        self.ui.camera_section_label.setPixmap(self._icons.camera.pixmap(25, 25, QIcon.Mode.Normal, QIcon.State.On))
+        self.ui.start_button.setIcon(self._icons.start)
+        self.ui.stop_button.setIcon(self._icons.stop)
+        self.ui.pause_button.setIcon(self._icons.pause)
+        self.ui.show_display_button.setIcon(self._icons.display)
+
+    def _connect_signals(self):
+        self.signal_camera_grabber_status.connect(self._main_object.camera_grabber.run_status_changed)
+        self.signal_camera_close.connect(self._main_object.camera_grabber.close)
+        listener = self._main_object.camera_grabber.listener
+        if isinstance(listener, CameraListener):
+            listener.signal_camera_state_changed.connect(self._slot_camera_state_changed)
+
+        self.ui.start_button.clicked.connect(self.start_button_clicked)
+        self.ui.stop_button.clicked.connect(self.stop_button_clicked)
+        self.ui.pause_button.clicked.connect(self.pause_button_clicked)
+        self.ui.show_display_button.clicked.connect(self.show_camera_display)
+
+    @Slot(bool)
+    def _slot_camera_state_changed(self, state: CameraState) -> None:
+        if state == CameraState.STARTED:
+            self.ui.start_button.setEnabled(False)
+            self.ui.pause_button.setEnabled(True)
+            self.ui.stop_button.setEnabled(True)
+
+            self.ui.statusbar.showMessage("Active", -1)
+
+            if self._camera_display is None:
+                self._camera_display = CameraDisplay(self)
+                self._main_object.set_display(self._camera_display)
+                self._camera_display.setWindowIcon(self._icons.display)
+                self._create_sub_window(self._camera_display, True)
+
+        elif state == CameraState.STOPPED:
+            self.ui.start_button.setEnabled(True)
+            self.ui.pause_button.setEnabled(False)
+            self.ui.stop_button.setEnabled(True)
+
+            self.ui.statusbar.showMessage("Paused", -1)
+
+        elif state == CameraState.CLOSED:
+            self.ui.start_button.setEnabled(True)
+            self.ui.pause_button.setEnabled(False)
+            self.ui.stop_button.setEnabled(False)
+
+            self.ui.statusbar.showMessage("Stopped", -1)
+
+    @Slot()
+    def start_button_clicked(self):
+        if not self._main_object.camera_grabber.is_camera_opened:
+            self._show_camera_select_dialog()
+        self.signal_camera_grabber_status.emit(True)
+
+    @Slot()
+    def stop_button_clicked(self):
+        self.signal_camera_close.emit()
+
+    @Slot()
+    def pause_button_clicked(self):
+        self.signal_camera_grabber_status.emit(False)
+
+    @Slot()
+    def show_camera_display(self):
+        if self._camera_display is None:
+            self._camera_display = CameraDisplay(self)
+            self._main_object.set_display(self._camera_display)
+            self._camera_display.setWindowIcon(self._icons.display)
+        sub = self._create_sub_window(self._camera_display, False)
+        if sub.isHidden():
+            sub.show()
+        else:
+            sub.setHidden(True)
+
+    def _show_camera_select_dialog(self):
+        camera_select_dialog = CameraSelectDialog(self)
+        camera_select_dialog.set_selector(self._main_object.camera_selector)
+        camera_select_dialog.exec()
+
+    def _create_sub_window(self, widget: QWidget, show: bool = True) -> QMdiSubWindow:
+        sub_windows = self.ui.mdiArea.subWindowList()
+        for sub in sub_windows:
+            if sub.widget() == widget:
+                return sub
+        sub = QMdiSubWindow()
+        sub.setWidget(widget)
+        sub.setObjectName(widget.objectName())
+        sub.setWindowIcon(widget.windowIcon())
+        sub.setWindowTitle(widget.windowTitle())
+        self.ui.mdiArea.addSubWindow(sub)
+        if show:
+            sub.show()
+        return sub
---
+++ laser_beam_measurements/widgets/main/ui_main_window.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QGroupBox, QHBoxLayout,
+    QLabel, QMainWindow, QMdiArea, QMenuBar,
+    QPushButton, QSizePolicy, QSpacerItem, QStatusBar,
+    QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(862, 659)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.buttonsGroupBox = QGroupBox(self.centralwidget)
+        self.buttonsGroupBox.setObjectName(u"buttonsGroupBox")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.buttonsGroupBox.sizePolicy().hasHeightForWidth())
+        self.buttonsGroupBox.setSizePolicy(sizePolicy)
+        self.buttonsGroupBox.setMinimumSize(QSize(0, 40))
+        self.buttonsGroupBox.setMaximumSize(QSize(16777215, 40))
+        self.horizontalLayout = QHBoxLayout(self.buttonsGroupBox)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.camera_section_label = QLabel(self.buttonsGroupBox)
+        self.camera_section_label.setObjectName(u"camera_section_label")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.camera_section_label.sizePolicy().hasHeightForWidth())
+        self.camera_section_label.setSizePolicy(sizePolicy1)
+        self.camera_section_label.setMinimumSize(QSize(30, 30))
+        self.camera_section_label.setMaximumSize(QSize(30, 30))
+
+        self.horizontalLayout.addWidget(self.camera_section_label)
+
+        self.start_button = QPushButton(self.buttonsGroupBox)
+        self.start_button.setObjectName(u"start_button")
+        sizePolicy1.setHeightForWidth(self.start_button.sizePolicy().hasHeightForWidth())
+        self.start_button.setSizePolicy(sizePolicy1)
+        self.start_button.setMaximumSize(QSize(30, 30))
+        self.start_button.setIconSize(QSize(25, 25))
+        self.start_button.setCheckable(True)
+        self.start_button.setChecked(False)
+
+        self.horizontalLayout.addWidget(self.start_button)
+
+        self.pause_button = QPushButton(self.buttonsGroupBox)
+        self.pause_button.setObjectName(u"pause_button")
+        sizePolicy1.setHeightForWidth(self.pause_button.sizePolicy().hasHeightForWidth())
+        self.pause_button.setSizePolicy(sizePolicy1)
+        self.pause_button.setMaximumSize(QSize(30, 30))
+        self.pause_button.setIconSize(QSize(25, 25))
+        self.pause_button.setCheckable(True)
+        self.pause_button.setChecked(False)
+
+        self.horizontalLayout.addWidget(self.pause_button)
+
+        self.stop_button = QPushButton(self.buttonsGroupBox)
+        self.stop_button.setObjectName(u"stop_button")
+        sizePolicy1.setHeightForWidth(self.stop_button.sizePolicy().hasHeightForWidth())
+        self.stop_button.setSizePolicy(sizePolicy1)
+        self.stop_button.setMaximumSize(QSize(30, 30))
+        self.stop_button.setStyleSheet(u"")
+        self.stop_button.setIconSize(QSize(24, 24))
+        self.stop_button.setCheckable(False)
+
+        self.horizontalLayout.addWidget(self.stop_button)
+
+        self.show_display_button = QPushButton(self.buttonsGroupBox)
+        self.show_display_button.setObjectName(u"show_display_button")
+        sizePolicy1.setHeightForWidth(self.show_display_button.sizePolicy().hasHeightForWidth())
+        self.show_display_button.setSizePolicy(sizePolicy1)
+        self.show_display_button.setMaximumSize(QSize(30, 30))
+        self.show_display_button.setStyleSheet(u"")
+        self.show_display_button.setIconSize(QSize(24, 24))
+        self.show_display_button.setCheckable(False)
+        self.show_display_button.setChecked(False)
+
+        self.horizontalLayout.addWidget(self.show_display_button)
+
+        self.save_image_button = QPushButton(self.buttonsGroupBox)
+        self.save_image_button.setObjectName(u"save_image_button")
+        sizePolicy1.setHeightForWidth(self.save_image_button.sizePolicy().hasHeightForWidth())
+        self.save_image_button.setSizePolicy(sizePolicy1)
+        self.save_image_button.setMaximumSize(QSize(30, 30))
+        self.save_image_button.setStyleSheet(u"")
+        self.save_image_button.setIconSize(QSize(24, 24))
+        self.save_image_button.setCheckable(False)
+
+        self.horizontalLayout.addWidget(self.save_image_button)
+
+        self.line = QFrame(self.buttonsGroupBox)
+        self.line.setObjectName(u"line")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.line.sizePolicy().hasHeightForWidth())
+        self.line.setSizePolicy(sizePolicy2)
+        self.line.setMinimumSize(QSize(10, 0))
+        self.line.setFrameShape(QFrame.Shape.VLine)
+        self.line.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.horizontalLayout.addWidget(self.line)
+
+        self.horizontalSpacer = QSpacerItem(185, 18, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addWidget(self.buttonsGroupBox)
+
+        self.mdiArea = QMdiArea(self.centralwidget)
+        self.mdiArea.setObjectName(u"mdiArea")
+
+        self.verticalLayout.addWidget(self.mdiArea)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 862, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.buttonsGroupBox.setTitle("")
+#if QT_CONFIG(tooltip)
+        self.camera_section_label.setToolTip(QCoreApplication.translate("MainWindow", u"Camera Control Section", None))
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(whatsthis)
+        self.camera_section_label.setWhatsThis("")
+#endif // QT_CONFIG(whatsthis)
+        self.camera_section_label.setText("")
+#if QT_CONFIG(tooltip)
+        self.start_button.setToolTip(QCoreApplication.translate("MainWindow", u"Start", None))
+#endif // QT_CONFIG(tooltip)
+        self.start_button.setText("")
+#if QT_CONFIG(tooltip)
+        self.pause_button.setToolTip(QCoreApplication.translate("MainWindow", u"Pause", None))
+#endif // QT_CONFIG(tooltip)
+        self.pause_button.setText("")
+#if QT_CONFIG(tooltip)
+        self.stop_button.setToolTip(QCoreApplication.translate("MainWindow", u"Stop", None))
+#endif // QT_CONFIG(tooltip)
+        self.stop_button.setText("")
+#if QT_CONFIG(tooltip)
+        self.show_display_button.setToolTip(QCoreApplication.translate("MainWindow", u"Show Display", None))
+#endif // QT_CONFIG(tooltip)
+        self.show_display_button.setText("")
+#if QT_CONFIG(tooltip)
+        self.save_image_button.setToolTip(QCoreApplication.translate("MainWindow", u"Save Image", None))
+#endif // QT_CONFIG(tooltip)
+        self.save_image_button.setText("")
+    # retranslateUi
+
