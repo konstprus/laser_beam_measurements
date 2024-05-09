2734812097e2bebf2e2152028bd01e85272a2264
Konstantin Prusakov <konstantin.prusakov@phystech.edu>
2024-05-09 21:02:08 +0300

    add camera_property_controller

---
+++ laser_beam_measurements/widgets/camera_control/camera_property_controller_widget.py
@@ -0,0 +1,121 @@
+# 
+# Project: laser_beam_measurements
+#
+# File: camera_property_controller_widget.py
+#
+# Author: Konstantin Prusakov
+#
+# Copyright 2024 Konstantin Prusakov <konstantin.prusakov@phystech.edu>
+#
+
+# pyside6-uic laser_beam_measurements/widgets/camera_control/camera_property_controller_widget.ui -o laser_beam_measurements/widgets/camera_control/ui_camera_property_controller_widget.py
+
+
+from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Slot, Signal
+from laser_beam_measurements.camera_control.camera_property_controller import CameraPropertyController
+from laser_beam_measurements.camera_control.camera_base import CameraBase
+from laser_beam_measurements.camera_control.camera_property_base import CameraPropertyBase
+
+from .ui_camera_property_controller_widget import Ui_Form
+
+
+class CameraPropertyControllerWidget(QWidget):
+
+    signal_camera_property_changed = Signal(str, object)
+
+    def __init__(self, parent=None, property_controller: CameraPropertyController | None = None):
+        super(CameraPropertyControllerWidget, self).__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self._property_controller: CameraPropertyController | None = None
+        self._connect_signals()
+        if property_controller is not None:
+            self.set_controller(property_controller)
+
+    def set_controller(self, property_controller: CameraPropertyController) -> None:
+        if self._property_controller:
+            self._property_controller.signal_camera_changed.disconnect(self.slot_camera_changed)
+            self.signal_camera_property_changed.disconnect(self._property_controller.set_property_value)
+            self._property_controller.signal_property_value_changed.disconnect(self.slot_update_property_value)
+            self._property_controller.signal_camera_unset.disconnect(self.slot_camera_unset)
+        self._property_controller = property_controller
+        self._property_controller.signal_camera_changed.connect(self.slot_camera_changed)
+        self.signal_camera_property_changed.connect(self._property_controller.set_property_value)
+        self._property_controller.signal_property_value_changed.connect(self.slot_update_property_value)
+        self._property_controller.signal_camera_unset.connect(self.slot_camera_unset)
+
+    @Slot()
+    def slot_camera_changed(self) -> None:
+        self._update_properties()
+
+    def _update_properties(self, disable_all: bool = False) -> None:
+        if disable_all:
+            self.ui.fps_slider.setDisabled(True)
+            self.ui.gain_slider.setDisabled(True)
+            self.ui.exposure_slider.setDisabled(True)
+        controller = self._property_controller
+        if controller is not None and controller.available:
+            self.blockSignals(True)
+            if controller.has_property("fps"):
+                self.ui.fps_slider.setEnabled(True)
+                prop = controller.get_property("fps")
+                self.ui.fps_slider.setMinimum(prop.min)
+                self.ui.fps_slider.setMaximum(prop.max)
+                self.ui.fps_slider.setValue(prop.value)
+            else:
+                self.ui.fps_slider.setDisabled(True)
+
+            if controller.has_property("gain"):
+                self.ui.gain_slider.setEnabled(True)
+                prop = controller.get_property("gain")
+                self.ui.gain_slider.setMinimum(prop.min)
+                self.ui.gain_slider.setMaximum(prop.max)
+                self.ui.gain_slider.setValue(prop.value)
+            else:
+                self.ui.gain_slider.setDisabled(True)
+
+            if controller.has_property("exposure"):
+                self.ui.exposure_slider.setEnabled(True)
+                prop = controller.get_property("exposure")
+                self.ui.exposure_slider.setMinimum(prop.min)
+                self.ui.exposure_slider.setMaximum(prop.max)
+                self.ui.exposure_slider.setValue(prop.value)
+            else:
+                self.ui.exposure_slider.setDisabled(True)
+            self.blockSignals(False)
+
+    @Slot(str, object)
+    def slot_update_property_value(self, name: str, value: object) -> None:
+        self.blockSignals(True)
+        self._update_property_value(name, value)
+        self.blockSignals(False)
+
+    @Slot()
+    def slot_camera_unset(self) -> None:
+        self._update_properties(disable_all=True)
+
+    def _update_property_value(self, name: str, value: object) -> None:
+        if name == "fps":
+            self.ui.fps_slider.setValue(value)
+        elif name == "gain":
+            self.ui.gain_slider.setValue(value)
+        elif name == "exposure":
+            self.ui.exposure_slider.setValue(value)
+
+    def _connect_signals(self) -> None:
+        self.ui.exposure_slider.value_changed.connect(self._change_exposure_value)
+        self.ui.fps_slider.value_changed.connect(self._change_fps_value)
+        self.ui.gain_slider.value_changed.connect(self._change_gain_value)
+
+    @Slot(float)
+    def _change_exposure_value(self, value: float) -> None:
+        self.signal_camera_property_changed.emit("exposure", value)
+
+    @Slot(float)
+    def _change_fps_value(self, value: float) -> None:
+        self.signal_camera_property_changed.emit("fps", value)
+
+    @Slot(float)
+    def _change_gain_value(self, value: float) -> None:
+        self.signal_camera_property_changed.emit("gain", value)
---
+++ laser_beam_measurements/widgets/camera_control/ui_camera_property_controller_widget.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'camera_property_controller_widget.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QGridLayout, QGroupBox,
+    QHBoxLayout, QHeaderView, QLabel, QSizePolicy,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
+
+from ..utils.slider_spin_box import SliderSpinBox
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(431, 217)
+        self.verticalLayout = QVBoxLayout(Form)
+        self.verticalLayout.setSpacing(2)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(5, 5, 5, 5)
+        self.camera_info_group_box = QGroupBox(Form)
+        self.camera_info_group_box.setObjectName(u"camera_info_group_box")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.camera_info_group_box.sizePolicy().hasHeightForWidth())
+        self.camera_info_group_box.setSizePolicy(sizePolicy)
+        self.camera_info_group_box.setMinimumSize(QSize(0, 105))
+        self.camera_info_group_box.setMaximumSize(QSize(16777215, 16777215))
+        self.horizontalLayout = QHBoxLayout(self.camera_info_group_box)
+        self.horizontalLayout.setSpacing(5)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(5, 0, 5, 5)
+        self.camera_info = QTableWidget(self.camera_info_group_box)
+        if (self.camera_info.columnCount() < 1):
+            self.camera_info.setColumnCount(1)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.camera_info.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        if (self.camera_info.rowCount() < 4):
+            self.camera_info.setRowCount(4)
+        font = QFont()
+        font.setBold(True)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        __qtablewidgetitem1.setFont(font);
+        self.camera_info.setVerticalHeaderItem(0, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        __qtablewidgetitem2.setFont(font);
+        self.camera_info.setVerticalHeaderItem(1, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        __qtablewidgetitem3.setFont(font);
+        self.camera_info.setVerticalHeaderItem(2, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        __qtablewidgetitem4.setFont(font);
+        self.camera_info.setVerticalHeaderItem(3, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.camera_info.setItem(0, 0, __qtablewidgetitem5)
+        self.camera_info.setObjectName(u"camera_info")
+        self.camera_info.setMinimumSize(QSize(0, 80))
+        self.camera_info.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.camera_info.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.camera_info.setSizeAdjustPolicy(QAbstractScrollArea.SizeAdjustPolicy.AdjustIgnored)
+        self.camera_info.setSortingEnabled(False)
+        self.camera_info.horizontalHeader().setVisible(False)
+        self.camera_info.horizontalHeader().setMinimumSectionSize(225)
+        self.camera_info.horizontalHeader().setDefaultSectionSize(10000)
+        self.camera_info.verticalHeader().setVisible(True)
+        self.camera_info.verticalHeader().setCascadingSectionResizes(False)
+        self.camera_info.verticalHeader().setMinimumSectionSize(20)
+        self.camera_info.verticalHeader().setDefaultSectionSize(20)
+
+        self.horizontalLayout.addWidget(self.camera_info)
+
+
+        self.verticalLayout.addWidget(self.camera_info_group_box)
+
+        self.camera_settings_group_box = QGroupBox(Form)
+        self.camera_settings_group_box.setObjectName(u"camera_settings_group_box")
+        self.camera_settings_group_box.setMinimumSize(QSize(0, 100))
+        self.camera_settings_group_box.setMaximumSize(QSize(16777215, 16777215))
+        self.gridLayout = QGridLayout(self.camera_settings_group_box)
+        self.gridLayout.setSpacing(1)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(2, 1, 2, 1)
+        self.label_4 = QLabel(self.camera_settings_group_box)
+        self.label_4.setObjectName(u"label_4")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.label_4.sizePolicy().hasHeightForWidth())
+        self.label_4.setSizePolicy(sizePolicy1)
+        self.label_4.setMinimumSize(QSize(80, 20))
+        self.label_4.setMaximumSize(QSize(80, 20))
+
+        self.gridLayout.addWidget(self.label_4, 0, 0, 1, 1)
+
+        self.gain_slider = SliderSpinBox(self.camera_settings_group_box)
+        self.gain_slider.setObjectName(u"gain_slider")
+        sizePolicy1.setHeightForWidth(self.gain_slider.sizePolicy().hasHeightForWidth())
+        self.gain_slider.setSizePolicy(sizePolicy1)
+        self.gain_slider.setMinimumSize(QSize(0, 20))
+        self.gain_slider.setMaximumSize(QSize(500, 20))
+
+        self.gridLayout.addWidget(self.gain_slider, 0, 1, 1, 1)
+
+        self.label_2 = QLabel(self.camera_settings_group_box)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy2)
+        self.label_2.setMinimumSize(QSize(80, 20))
+        self.label_2.setMaximumSize(QSize(80, 20))
+
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+
+        self.exposure_slider = SliderSpinBox(self.camera_settings_group_box)
+        self.exposure_slider.setObjectName(u"exposure_slider")
+        sizePolicy1.setHeightForWidth(self.exposure_slider.sizePolicy().hasHeightForWidth())
+        self.exposure_slider.setSizePolicy(sizePolicy1)
+        self.exposure_slider.setMinimumSize(QSize(180, 20))
+        self.exposure_slider.setMaximumSize(QSize(500, 20))
+
+        self.gridLayout.addWidget(self.exposure_slider, 1, 1, 1, 1)
+
+        self.label_3 = QLabel(self.camera_settings_group_box)
+        self.label_3.setObjectName(u"label_3")
+        sizePolicy2.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy2)
+        self.label_3.setMinimumSize(QSize(80, 10))
+        self.label_3.setMaximumSize(QSize(80, 20))
+
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.fps_slider = SliderSpinBox(self.camera_settings_group_box)
+        self.fps_slider.setObjectName(u"fps_slider")
+        sizePolicy1.setHeightForWidth(self.fps_slider.sizePolicy().hasHeightForWidth())
+        self.fps_slider.setSizePolicy(sizePolicy1)
+        self.fps_slider.setMinimumSize(QSize(180, 20))
+        self.fps_slider.setMaximumSize(QSize(500, 20))
+
+        self.gridLayout.addWidget(self.fps_slider, 2, 1, 1, 1)
+
+
+        self.verticalLayout.addWidget(self.camera_settings_group_box)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.camera_info_group_box.setTitle(QCoreApplication.translate("Form", u"Camera Info", None))
+        ___qtablewidgetitem = self.camera_info.verticalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Form", u"Type:", None));
+        ___qtablewidgetitem1 = self.camera_info.verticalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Form", u"Name:", None));
+        ___qtablewidgetitem2 = self.camera_info.verticalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Form", u"Matrix Shape:", None));
+        ___qtablewidgetitem3 = self.camera_info.verticalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("Form", u"Pixel Size:", None));
+
+        __sortingEnabled = self.camera_info.isSortingEnabled()
+        self.camera_info.setSortingEnabled(False)
+        self.camera_info.setSortingEnabled(__sortingEnabled)
+
+        self.camera_settings_group_box.setTitle(QCoreApplication.translate("Form", u"Camera Settings", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"Gain", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"Exposure", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"FPS", None))
+    # retranslateUi
+
