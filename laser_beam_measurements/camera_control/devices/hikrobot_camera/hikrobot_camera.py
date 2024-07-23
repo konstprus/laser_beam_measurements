c90dfd97a8c93e9439d40517beda00bd4300aa3f
Konstantin Prusakov <konstantin.prusakov@phystech.edu>
2024-07-23 20:06:32 +0300

    initial files for hikrobot camera implementation

---
+++ laser_beam_measurements/camera_control/devices/hikrobot_camera/hikrobot_camera.py
@@ -0,0 +1,9 @@
+# 
+# Project: laser_beam_measurements
+#
+# File: hikrobot_camera.py
+#
+# Author: Konstantin Prusakov
+#
+# Copyright 2024 Konstantin Prusakov <konstantin.prusakov@phystech.edu>
+#
