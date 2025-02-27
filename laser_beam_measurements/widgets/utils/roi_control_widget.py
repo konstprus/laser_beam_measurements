74774af878ad4ba2d67349e56d3ad5f75bb2222c
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2025-02-27 16:53:24 +0300

    Widget for adjusting the ROI parameter was added.

---
+++ laser_beam_measurements/widgets/utils/roi_control_widget.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QWidget
+
+from ..utils.ui_roi_control_widget import Ui_Form
+
+class ROIControl(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+    def set_label_text(self, text: str) -> None:
+        self.ui.label.setText(text)
